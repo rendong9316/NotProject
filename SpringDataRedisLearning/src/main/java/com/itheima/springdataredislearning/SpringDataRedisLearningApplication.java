0dd6a8291c9efd1f6ede7e1ec4f21f7ad9c9f7c4
rendong9316 <rendong9316@163.com>
2026-07-30 10:28:35 +0800

    SpringDataRedisLearning:项目骨架搭建完毕

---
+++ SpringDataRedisLearning/src/main/java/com/itheima/springdataredislearning/SpringDataRedisLearningApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springdataredislearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRedisLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRedisLearningApplication.class, args);
+    }
+
+}
---
+++ SpringDataRedisLearning/src/test/java/com/itheima/springdataredislearning/SpringDataRedisLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springdataredislearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
