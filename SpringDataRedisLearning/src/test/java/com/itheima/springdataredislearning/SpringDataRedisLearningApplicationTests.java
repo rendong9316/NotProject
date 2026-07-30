231e369552f990d9ba53eb83e31843fe088977fb
rendong9316 <rendong9316@163.com>
2026-07-30 10:49:40 +0800

    SpringDataRedisLearning:redis template 简单使用，连接测试

--- SpringDataRedisLearning/src/test/java/com/itheima/springdataredislearning/SpringDataRedisLearningApplicationTests.java
+++ SpringDataRedisLearning/src/test/java/com/itheima/springdataredislearning/SpringDataRedisLearningApplicationTests.java
@@ -1,13 +1,21 @@
 package com.itheima.springdataredislearning;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class SpringDataRedisLearningApplicationTests {
+    @Autowired
+    //注入
+    private RedisTemplate redisTemplate;
 
     @Test
-    void contextLoads() {
+    void testString() {
+        redisTemplate.opsForValue().set("name","rend");
+        Object result =redisTemplate.opsForValue().get("name");
+        System.out.println(result);
     }
 
 }
