e6107caedc6b34c0d5731f8741ad890c337c1efc
rendong9316 <rendong9316@163.com>
2026-08-03 15:54:48 +0800

    skytakeout:启用禁用员工账号

--- SkyTake_Out/sky-take-out/sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ SkyTake_Out/sky-take-out/sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -22,5 +22,17 @@ public interface EmployeeService {
      */
     void save(EmployeeDTO employeeDTO);
 
+    /**
+     * 分页查询员工
+     * @param employeePageQueryDTO
+     * @return
+     */
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
 }
--- SkyTake_Out/sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ SkyTake_Out/sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -110,5 +110,19 @@ public class EmployeeServiceImpl implements EmployeeService {
         return result;
     }
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    public void startOrStop(Integer status, Long id) {
+        // update employee set status = ? where id = ?
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+        employeeMapper.update(employee);
+    }
+
 
 }
