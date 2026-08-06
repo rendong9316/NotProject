dea79d8eceb30d85355316730dabe40b5c8324e7
rendong9316 <rendong9316@163.com>
2026-08-06 16:06:38 +0800

    sky-take-out：新增菜品部分实现完全

--- SkyTake_Out/sky-take-out/sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ SkyTake_Out/sky-take-out/sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,4 +1,51 @@
 package com.sky.service.impl;
 
-public class DishServiceImpl {
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class DishServiceImpl implements DishService {
+
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+
+
+    /**
+     * 新增菜品和口味
+     * @param dishDTO
+     */
+    @Transactional   //事务注解：让方法原子性，涉及夺标插入数据，必须要么全成功要么全失败
+    public void saveWithFlavor(DishDTO dishDTO) {
+
+        //插入一条菜品数据
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+        dishMapper.insert(dish);
+        //获取insert语句生成的主键值
+        Long dishId = dish.getId();
+        //插入n条口味数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if(flavors!=null&&flavors.size()>0){
+            for (DishFlavor dishFlavor : flavors) {
+                dishFlavor.setDishId(dishId);
+            }
+            dishFlavorMapper.insertbatch(flavors);
+        }
+
+    }
 }
