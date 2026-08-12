12354b3c4500ef72f32d90db2dbda071e0c9cbfb
rendong9316 <rendong9316@163.com>
2026-08-12 19:44:04 +0800

    gitignore

---
+++ Leetcode_Learning/src/com/leetcode/stackAndQueue/MyStack.java
@@ -0,0 +1,39 @@
+package com.leetcode.stackAndQueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public boolean empty() {
+
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
