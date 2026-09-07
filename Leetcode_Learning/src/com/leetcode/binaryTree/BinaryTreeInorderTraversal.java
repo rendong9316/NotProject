7f3bfb14ddc3648863a5c87ab7e111e242c7c12f
rendong9316 <rendong9316@163.com>
2026-09-07 15:59:37 +0800

    LeetCode：二叉树中序遍历（递归实现）比较简单，有一些自己关于递归的心得体会

---
+++ Leetcode_Learning/src/com/leetcode/binaryTree/BinaryTreeInorderTraversal.java
@@ -0,0 +1,78 @@
+package com.leetcode.binaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ **/
+    public List<Integer> inorderTraversal(TreeNode root) {
+        //左右中
+        List<Integer> list = new ArrayList<>();
+        diguiF(root,list);
+        return list;
+        }
+
+    private void diguiF(TreeNode cur,List list){
+
+        //关于递归的一点心得体会
+        //整体就三件事：先输出当前树的根节点，然后对左子树执行遍历，然后对右子树进行遍历
+        //相信diguiF能实现。相信的原理是有一个list.add(cur.val)步骤，有这个就能保证不断递归每个节点都有机会
+        //成为root，从而依次被写入list
+        if(cur==null){
+            return;
+        }
+
+        diguiF(cur.left,list);
+        list.add(cur.val);
+        diguiF(cur.right,list);
+
+    }
+
+
+
+    public static void main(String[] args) {
+        // 1. 手动构建一棵二叉树
+        // 例如构建：    1
+        //             / \
+        //            2   3
+        //           / \
+        //          4   5
+
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node2 = new TreeNode(2, node4, node5);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode root = new TreeNode(1, node2, node3);
+
+        // 2. 创建测试对象，调用方法
+        BinaryTreeInorderTraversal solution = new BinaryTreeInorderTraversal();
+        List<Integer> result = solution.inorderTraversal(root);
+
+        // 3. 打印结果
+        System.out.println("前序遍历结果：" + result);
+        // 期望输出：[1, 2, 4, 5, 3]
+    }
+
+
+
+    }
+
+
+
+
+
