c7f35722e84007ae3b75847460a240d40f7fb3d9
rendong9316 <rendong9316@163.com>
2026-09-09 19:56:15 +0800

    sky-take-out:微信登陆功能实现

---
+++ SkyTake_Out/sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    /**
+     * 根据openid查询用户
+     * @param openid
+     * @return
+     */
+    @Select("select * from sky_take_out.user where openid = #{openid}")
+    User getByOpenid(String openid);
+
+    @Insert("insert into sky_take_out.user (openid, name, phone, sex, id_number, avatar, create_time) " +
+            "values (#{openid}, #{name}, #{phone}, #{sex}, #{idNumber}, #{avatar}, #{createTime})")
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    void insert(User user);
+}
